ef9a0c422f8af826f7b9e6c17b83b45f4a8b112d
ZexinZong <21112007@bjtu.edu.cn>
2025-12-03 12:51:56 +0800

    Add files via upload

--- tools/zzx_run_train_lion_manba_for_kitti.sh
+++ tools/zzx_run_train_lion_manba_for_kitti.sh
@@ -5,8 +5,8 @@
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
 --nproc_per_node=1 --master_port=29988 train.py  --tcp_port 29988  --launcher pytorch  \
 --cfg_file ./cfgs/kitti_models/zzx_second_with_lion_mamba_64dim.yaml \
---extra_tag zzx_second_with_lion_mamba_64dim_ep40 \
---batch_size 2  --epochs 40 --max_ckpt_save_num 10 --workers 2 --sync_bn
+--extra_tag bs4_zzx_second_with_lion_mamba_64dim \
+--batch_size 4  --epochs 4 --max_ckpt_save_num 40 --workers 4 --sync_bn
 
 
 
